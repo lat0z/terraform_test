9b9b19397afba33ada48229f118036f948b78525
luis <latoz@protonmail.com>
2021-08-01 21:41:20 -0500

    adding explicit versions to ensure reproducibility

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0.3"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.52.0"
+    }
+  }
+}
+
 #Specify the provider required for the test
 provider "aws" {
   region = "us-east-2"
@@ -5,21 +15,21 @@ provider "aws" {
 
 #Data source used to remove the hard coded ami id and use the latest one automatically
 data "aws_ami" "amazon" {
-    most_recent = true 
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
-    }
+  most_recent = true 
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
 }
 
 #Variable used to remove the explicit tags in each resource 
 variable "general_tags"{
-    type = map(string)
-    default = {
-        Name  = "Flugel"
-        Owner = "InfraTeam"
-    }
+  type = map(string)
+  default = {
+    Name  = "Flugel"
+    Owner = "InfraTeam"
+  }
 }
 
 #setting up instance for the test, it is using Amazon linux 
